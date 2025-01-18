d8bea4ec93fd06c620c6c5c0164efc85a232b776
Melissa Andrade Garcia Da Silva <meandrad@c2r7p2.42sp.org.br>
2025-01-18 17:00:13 -0300

    .h corrected

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:35:01 by meandrad          #+#    #+#             */
-/*   Updated: 2025/01/18 16:57:08 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:59:36 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,13 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-void	child2_process(int *fd, char *argv[], char *envp[]);
-void	child1_process(int *fd, char *argv[], char *envp[]);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	**ft_split(char const *s, char c);
+
+void	child1_process(int *fd, char *argv[], char *envp[]);
+void	child2_process(int *fd, char *argv[], char *envp[]);
 void	cmd_execute(char *argv, char *envp[]);
 char	*find_path(char *cmd, char *envp[]);
-char	**ft_split(char const *s, char c);
 void	handle_error(int error_code);
 void	free_path(char **path);
 
