66e20fa966ea75fd00013afaf2826ad7a2729c77
Melissa Andrade Garcia Da Silva <meandrad@c2r7p2.42sp.org.br>
2025-01-18 16:57:51 -0300

    .h corrected

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: meandrad <meandrad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:35:01 by meandrad          #+#    #+#             */
-/*   Updated: 2025/01/12 13:30:13 by meandrad         ###   ########.fr       */
+/*   Updated: 2025/01/18 16:57:08 by meandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 void	cmd_execute(char *argv, char *envp[]);
 char	*find_path(char *cmd, char *envp[]);
 char	**ft_split(char const *s, char c);
-void	handle_error(int index);
+void	handle_error(int error_code);
 void	free_path(char **path);
 
 #endif
